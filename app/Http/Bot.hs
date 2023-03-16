c99471c5c6455694f2a3d84ccb815a3ed0c28109
Jeakin <Jeakin@botu.cc>
2023-03-16 16:43:29 +0800

    fix

--- app/Http/Bot.hs
+++ app/Http/Bot.hs
@@ -2,21 +2,22 @@
 
 module Http.Bot where
 
-import Http.Types
 import Control.Monad.IO.Class
+import Http.Types
 
 callBack :: (MonadIO m) => String -> BotReq -> m (Resp Message)
 callBack name msg = do
   liftIO $ print name >> print msg
-  pure Resp
-    { message = "success",
-      code = 0,
-      respData =
-        Message
-          { messageType = "bot",
-            info =
-              Info
-                { text = "body"
-                }
-          }
-    }
+  pure
+    Resp
+      { message = "success",
+        code = 0,
+        respData =
+          Message
+            { messageType = 500,
+              info =
+                Info
+                  { text = "body"
+                  }
+            }
+      }
